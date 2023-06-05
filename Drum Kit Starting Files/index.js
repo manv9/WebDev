785d2aaa054ad932ef3908a52fdd266ef6900c93
manv9 <tirumalmanav4@gmail.com>
2023-06-05 12:20:59 +0530

    aa

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,1 +1,8 @@
-alert("hello");
+var numberofdrumbuttons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberofdrumbuttons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I Got Clicked");
+    });
+}
